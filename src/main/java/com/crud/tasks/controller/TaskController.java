401db9d1f41785ed12444166b3b14e758135e091
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-01-21 16:31:38 +0100

    Exc 21.6 not done

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -44,7 +44,7 @@ public class TaskController {
         return ResponseEntity.ok(taskMapper.mapToTaskDto(savedTask));
     }
 
-    @PostMapping(value = "/createTask")
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, value = "/createTask")
     public ResponseEntity<Void> createTask(@RequestBody TaskDto taskDto) {
         Task task = taskMapper.mapToTask(taskDto);
         service.saveTask(task);
