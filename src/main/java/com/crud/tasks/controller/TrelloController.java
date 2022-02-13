3cf1439fbf050a8823149325428a8a13131b488b
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-02-13 18:28:13 +0100

    @Cross Origin modified in TrelloController

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/trello")
 @RequiredArgsConstructor
